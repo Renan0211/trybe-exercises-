e001d714843479d18d33aa7077f8015fa6ab78fa
Renan Antunes <renan.antunes.rocha@gmail.com>
2021-02-24 20:49:48 -0300

    Adiciona o exercicio 10

---
+++ Fundamentos-Do-Desenvolvimento-Web/bloco_4/dia_1/exercicio_do_dia/10.js
@@ -0,0 +1,7 @@
+let valorCusto =  100;
+let impostoSobreOCusto = 20/100;
+let valorCustoTotal = valorCusto + (valorCusto * impostoSobreOCusto);
+let valorVenda = 150;
+let lucro = valorVenda - valorCustoTotal;
+
+console.log(lucro);
