f5d0ac6340709b3225342be2448075849d7b0a3c
Renan Antunes <renan.antunes.rocha@gmail.com>
2021-02-24 20:34:21 -0300

    Adiciona o exercicio 7

---
+++ Fundamentos-Do-Desenvolvimento-Web/bloco_4/dia_1/exercicio_do_dia/7.js
@@ -0,0 +1,23 @@
+let grade = 59;
+
+if (grade >= 90 && grade < 100) {
+    console.log("A");
+}
+else if (grade >= 80 && grade < 90) {
+    console.log("B");
+}
+else if (grade >= 70 && grade < 80) {
+    console.log("C");
+}
+else if (grade >= 60 && grade < 70) {
+    console.log("D");
+}
+else if (grade >= 50 && grade < 60) {
+    console.log("E");
+}
+else if (grade < 50 && grade >= 0) {
+    console.log("F");
+}
+else {
+    console.log("Valor inválido")
+}
