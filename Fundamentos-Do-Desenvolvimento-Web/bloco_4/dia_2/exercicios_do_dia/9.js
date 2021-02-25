89bb30799420602bb1ccb33ae94328270bbd981a
Renan Antunes <renan.antunes.rocha@gmail.com>
2021-02-25 16:10:37 -0300

    Adiciona o exercicio 9

---
+++ Fundamentos-Do-Desenvolvimento-Web/bloco_4/dia_2/exercicios_do_dia/9.js
@@ -0,0 +1,9 @@
+let numbers = [];
+
+for(let contagem = 1; contagem <= 25; contagem++) {
+    numbers.push(contagem);
+}
+
+for(let index = 0; index < numbers.length; index++) {
+    console.log(numbers[index] / 2);
+}
