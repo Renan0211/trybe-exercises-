c967f53615a2ca18d578dacf2a5aacd4628c5707
Renan Antunes <renan.antunes.rocha@gmail.com>
2021-03-08 16:43:54 -0300

    Adiciona o exercicio 1

--- Fundamentos-Do-Desenvolvimento-Web/bloco_5/dia_3/script.js
+++ Fundamentos-Do-Desenvolvimento-Web/bloco_5/dia_3/script.js
@@ -18,7 +18,56 @@ function createDaysOfTheWeek() {
     weekDaysList.appendChild(dayListItem);
   }
 }
-
+function createDaysOfTheMonth() {
+  const dezDaysList = [
+    29,
+    30,
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    11,
+    12,
+    13,
+    14,
+    15,
+    16,
+    17,
+    18,
+    19,
+    20,
+    21,
+    22,
+    23,
+    24,
+    25,
+    26,
+    27,
+    28,
+    29,
+    30,
+    31,
+  ];
+  let listOfMonthDays = document.getElementById("days");
+  for (i = 0; i < dezDaysList.length; i += 1) {
+    let newDay = document.createElement("li");
+    newDay.className = "day";
+    if (dezDaysList[i] === 24 || 25 || 31) {
+      newDay.className = " holiday";
+    }
+    if (dezDaysList[i] === 4 || 11 || 18 || 25) {
+      newDay.className += " friday";
+    }
+    newDay.innerText = dezDaysList[i];
+    listOfMonthDays.appendChild(newDay);
+  }
+}
 createDaysOfTheWeek();
-
+createDaysOfTheMonth();
 // Escreva seu código abaixo.
