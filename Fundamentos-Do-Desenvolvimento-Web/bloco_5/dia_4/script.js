56e34ceebb4302ca4f83f2f8ed8640a603e7e861
Renan Antunes <renan.antunes.rocha@gmail.com>
2021-03-09 12:08:29 -0300

    Adiciona o ex 2

--- Fundamentos-Do-Desenvolvimento-Web/bloco_5/dia_4/script.js
+++ Fundamentos-Do-Desenvolvimento-Web/bloco_5/dia_4/script.js
@@ -5,7 +5,7 @@
 // Tipo da fonte ( Font family ).
 
 localStorage.setItem("backgroundColor","#f3f3f3")
-localStorage.setItem("fontColor", "black")
+localStorage.setItem("fontColor", "blue")
 localStorage.setItem("fontSize","25px")
 localStorage.setItem("lineHeight","1.6")
 localStorage.setItem("fontFamily","monospace")
@@ -14,4 +14,9 @@ function changeBackgroundColor() {
   document.body.style.backgroundColor = localStorage.getItem("backgroundColor")
 }
 
+function changeFontColor () {
+  document.body.style.color = localStorage.getItem("fontColor")
+}
+
 changeBackgroundColor()
+changeFontColor()
