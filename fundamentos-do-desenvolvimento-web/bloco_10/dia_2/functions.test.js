14a4524630b5d8a2dcb2e0e5b0cbc0c4234304fe
Renan Antunes <renan.antunes.rocha@gmail.com>
2021-04-23 11:09:15 -0300

    Implementa testes para a funçao getUserName e findUserById usando o then/catch

--- fundamentos-do-desenvolvimento-web/bloco_10/dia_2/functions.test.js
+++ fundamentos-do-desenvolvimento-web/bloco_10/dia_2/functions.test.js
@@ -1,9 +1,28 @@
-const uppercase = (str, callback) => {
-  callback(str.toUpperCase());
-}
+
+const {uppercase, findUserById, getUserName} = require('./functions')
+
 test('se uppercase teste é igual à TESTE', (done) => {
   uppercase('teste', (str) => {
     expect(str).toBe('TESTE')
     done();
+  })
+})
+
+
+describe('when the user is found by the id', () => {
+  test('if is returned the right user', () => {
+    expect.assertions(1);
+    return getUserName(4).then(user => {
+      expect(user).toBe('Mark');
+    })
+  })
+})
+
+describe('whent the users id doesnt exist', () => {
+  test('return an error', () => {
+    expect.assertions(1);
+    return getUserName(8).catch(error => {
+      expect(error).toEqual({error: 'User with 8 not found.'})
+    })
   })
 })
