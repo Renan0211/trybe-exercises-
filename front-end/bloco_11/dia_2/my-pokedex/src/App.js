d8badfe7732c89b91b8b3d4632256eb7de96310b
Renan Antunes <renan.antunes.rocha@gmail.com>
2021-04-28 13:01:01 -0300

    Commit bomba

--- front-end/bloco_11/dia_2/my-pokedex/src/App.js
+++ front-end/bloco_11/dia_2/my-pokedex/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import Pokedex from './Pokedex';
+import './bulma.css';
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section class="hero is-medium is-warning">
+        <div class="hero-body">
+          <p class="title">POKÉDEX</p>
+        </div>
+      </section>
+      <div className="container is-warning">
+        <Pokedex />
+      </div>
     </div>
   );
 }
